87a407b0d72c6707f9d8b9a349bb8e4831df884f
YuriySga <ess1928@yandex.ru>
2020-03-08 21:31:01 +0400

    feat: arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) { 
+    if (Array.isArray(arr) == false || arr.length === 0) return 0;
+    arr.sort( (a, b) => a - b );
+    return arr[0];  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (Array.isArray(arr) == false || arr.length === 0) return 0;
+  arr.sort( (a, b) => a - b );
+  return arr[arr.length-1];  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (Array.isArray(arr) == false || arr.length === 0) return 0;
+  let result = arr.reduce((sum, current) => sum + current, 0);
+  return result/arr.length;
 }
